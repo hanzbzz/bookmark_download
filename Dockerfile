6ff063e04eb5c17c6c18496c1fec751306cac15c
janbrazda <xbrazda@fi.muni.cz>
2022-10-03 13:56:10 +0200

    feat: add docker support

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM python:3.10-alpine
+WORKDIR /usr/app
+COPY ./ /usr/app
+RUN pip3 install -r requirements.txt
+RUN apk add --update npm
+RUN npm install
+EXPOSE 8080
+CMD ["waitress-serve", "--call", "--port=8080", "downloader.app:create_app"]
---
+++ downloader/app.py
@@ -0,0 +1,31 @@
+
+import os
+
+from flask import Flask, render_template, session, g
+from . import auth, api
+
+def create_app():
+    app = Flask(__name__,static_url_path="/static")
+    app.config.from_mapping(
+        SECRET_KEY='dev'
+    )
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.route('/')
+    def index():
+        logged_in = session.get('bearer_token') is not None
+        return render_template('user.html', logged_in=True) if logged_in else render_template('login.html', logged_in=False)
+
+    app.register_blueprint(auth.bp)
+    app.register_blueprint(api.bp)
+
+    app.permanent_session_lifetime = 60 * 60 * 24 
+    @app.before_request
+    def permanent_session():
+        g.username, g.profile_pic_url = session.get('username'), session.get('profile_pic_url')
+        session.permanent = True
+    
+    return app
