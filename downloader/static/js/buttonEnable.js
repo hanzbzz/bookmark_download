4fde6cd7f4538ccd9da4fe24e56eaf43a4ca5073
janbrazda <xbrazda@fi.muni.cz>
2022-10-03 11:00:28 +0200

    feat: add icon

--- downloader/static/js/buttonEnable.js
+++
@@ -1,3 +0,0 @@
-if (document.getElementById("photo").checked || document.getElementById("text").checked || document.getElementById("video").checked ){
-    document.getElementById("submit").removeAttribute("disabled")
-}
